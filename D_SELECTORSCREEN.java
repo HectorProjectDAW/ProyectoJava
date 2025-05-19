486c3d51db6e3075a48d6fa4217447b8aa16a35a
HectorProjectDAW <eleuteriodaw@gmail.com>
2025-05-19 10:09:44 +0200

    Update D_SELECTORSCREEN.java
    19/5/25
    10:10

--- D_SELECTORSCREEN.java
+++ D_SELECTORSCREEN.java
@@ -1,15 +1,11 @@
 package com.proyecto.mi_proyecto;
 
-package cosaproycto;
-
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.List;
 import java.awt.EventQueue;
+import java.util.List;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -21,26 +17,27 @@ import javax.swing.border.EmptyBorder;
 public class D_SelectorTemasScreen extends JFrame {
 
     private static final long serialVersionUID = 1L;
-    private JPanel contentPane;
+    protected JPanel contentPane;
+    protected JButton btnTema1, btnTema2, btnTema3;
+    protected JLabel lblTema1, lblTema2, lblTema3;
 
     private String convertirNombreArchivo(String tematica) {
         String nombre = tematica.toLowerCase()
-            .replace("á", "a")
-            .replace("é", "e")
-            .replace("í", "i")
-            .replace("ó", "o")
-            .replace("ú", "u")
-            .replace("ñ", "n")
+            .replace("á", "a").replace("é", "e").replace("í", "i")
+            .replace("ó", "o").replace("ú", "u").replace("ñ", "n")
             .replaceAll("[^a-z0-9]", "_");
         return nombre + ".png";
     }
 
     private void ajustarEtiqueta(JLabel etiqueta, String texto) {
+    	//Fuente del Label
         Font fuente = etiqueta.getFont();
+        //Saber cuanto ocupa el texto
         FontMetrics fm = etiqueta.getFontMetrics(fuente);
+        //Ancho texto y alto para la etiqueta
         int textoWidth = fm.stringWidth(texto);
         int width = etiqueta.getWidth();
-
+        //Si el texto es mas grande que la etiqueta, disminuye tamaño
         if (textoWidth > width) {
             int newSize = fuente.getSize() - 1;
             while (newSize > 8 && fm.stringWidth(texto) > width) {
@@ -48,6 +45,7 @@ public class D_SelectorTemasScreen extends JFrame {
                 fm = etiqueta.getFontMetrics(fuente);
                 newSize--;
             }
+            //Se aplica el nuevo texto
             etiqueta.setFont(fuente);
         }
     }
@@ -64,7 +62,6 @@ public class D_SelectorTemasScreen extends JFrame {
     }
 
     public D_SelectorTemasScreen() {
-        setBackground(new Color(139, 0, 0));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 550, 350);
         setTitle("Selector de Temáticas");
@@ -76,72 +73,82 @@ public class D_SelectorTemasScreen extends JFrame {
         contentPane.setLayout(null);
 
         JLabel lblTitulo = new JLabel("Selecciona la temática");
-        lblTitulo.setForeground(Color.BLACK);
         lblTitulo.setFont(new Font("Dialog", Font.BOLD, 18));
         lblTitulo.setBounds(155, 20, 250, 30);
         contentPane.add(lblTitulo);
 
-        // Obtener temáticas aleatorias de MongoDB
-        List<String> tematicas = TematicaMongo.Temas(3);
+        
+        btnTema1 = new JButton();
+        btnTema1.setBounds(29, 64, 150, 169);
+        contentPane.add(btnTema1);
 
-        int anchoBoton = 150;
-        int altoBoton = 169;
-        int inicioX = 50;
-        int espacio = 170;
-        int yImagen = 64;
-        int yTexto = yImagen + altoBoton + 5;
+        lblTema1 = new JLabel("", JLabel.CENTER);
+        lblTema1.setBounds(50, 240, 150, 20);
+        contentPane.add(lblTema1);
+
+        btnTema2 = new JButton();
+        btnTema2.setBounds(200, 62, 150, 169);
+        contentPane.add(btnTema2);
+
+        lblTema2 = new JLabel("", JLabel.CENTER);
+        lblTema2.setBounds(210, 240, 150, 20);
+        contentPane.add(lblTema2);
+
+        btnTema3 = new JButton();
+        btnTema3.setBounds(370, 64, 150, 169);
+        contentPane.add(btnTema3);
+
+        lblTema3 = new JLabel("", JLabel.CENTER);
+        lblTema3.setBounds(370, 240, 150, 20);
+        contentPane.add(lblTema3);
+
+        //Tematicas de tematicamongo.java. Hacer arrays 
+        List<String> tematicas = TematicaMongo.Temas(3);
+        JButton[] botones = { btnTema1, btnTema2, btnTema3 };
+        JLabel[] etiquetas = { lblTema1, lblTema2, lblTema3 };
 
         for (int i = 0; i < tematicas.size(); i++) {
             String tema = tematicas.get(i);
-            int x = inicioX + i * espacio;
+            botones[i].setBorderPainted(false);
+            botones[i].setContentAreaFilled(false);
+            etiquetas[i].setText(tema);
+            ajustarEtiqueta(etiquetas[i], tema);
 
-            // Crear botón con imagen
-            JButton boton = crearBotonImagen(tema, x, yImagen, anchoBoton, altoBoton);
-            contentPane.add(boton);
-
-            // Crear etiqueta con el nombre debajo del botón
-            JLabel etiquetaTema = new JLabel(tema, JLabel.CENTER);
-            etiquetaTema.setBounds(x, yTexto, anchoBoton, 20);
-            ajustarEtiqueta(etiquetaTema, tema);
-            contentPane.add(etiquetaTema);
-        }
-
-        // Botón Salir
-        JButton btnSalirAlMen = new JButton("Salir al menú");
-        btnSalirAlMen.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                C_MenuPrincipalScreen menu = new C_MenuPrincipalScreen();
-                menu.setVisible(true);
-                dispose();
+            // Buscar imagen
+            String ruta = "/img/" + convertirNombreArchivo(tema);
+            
+            //Buscar imagen dentro del proyecto
+            java.net.URL imgUrl = getClass().getResource(ruta);
+            if (imgUrl != null) {
+            	
+            	//Crear icono a partir de la imagen
+                ImageIcon iconoOriginal = new ImageIcon(imgUrl);
+                
+                //Escalar imagen para que salga como el boton
+                Image imagenEscalada = iconoOriginal.getImage().getScaledInstance(
+                        botones[i].getWidth(), botones[i].getHeight(), Image.SCALE_SMOOTH);
+                botones[i].setIcon(new ImageIcon(imagenEscalada));
+            } else {
+                System.err.println("No se encontró la imagen: " + ruta);
             }
-        });
-        btnSalirAlMen.setBounds(200, 265, 140, 30);
-        contentPane.add(btnSalirAlMen);
-    }
 
-    private JButton crearBotonImagen(String tema, int x, int y, int ancho, int alto) {
-        JButton btnImagen = new JButton();
-
-        String ruta = "/img/" + convertirNombreArchivo(tema);
-        java.net.URL imgUrl = getClass().getResource(ruta);
-
-        if (imgUrl != null) {
-            ImageIcon originalIcon = new ImageIcon(imgUrl);
-            Image imagenEscalada = originalIcon.getImage().getScaledInstance(ancho, alto, Image.SCALE_SMOOTH);
-            btnImagen.setIcon(new ImageIcon(imagenEscalada));
-        } else {
-            System.err.println("No se encontró la imagen: " + ruta);
+            // Acción del botón
+            botones[i].addActionListener(e -> {
+            	// Crear ventana del juego pasando la temática seleccionada
+                E_JuegoScreen juego = new E_JuegoScreen(tema);
+                juego.setVisible(true);
+                dispose();
+            });
         }
 
-        btnImagen.setBounds(x, y, ancho, alto);
-        btnImagen.setBorderPainted(false);
-        btnImagen.setContentAreaFilled(false);
-
-        btnImagen.addActionListener(e -> {
-            new E_JuegoScreen().setVisible(true);
+        //Salir
+        JButton btnSalirAlMen = new JButton("Salir al menú");
+        btnSalirAlMen.addActionListener(e -> {
+            C_MenuPrincipalScreen menu = new C_MenuPrincipalScreen();
+            menu.setVisible(true);
             dispose();
         });
-
-        return btnImagen;
+        btnSalirAlMen.setBounds(200, 280, 140, 30);
+        contentPane.add(btnSalirAlMen);
     }
 }
