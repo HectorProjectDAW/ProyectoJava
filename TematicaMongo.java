7ad574bddd31eadcc04a27bdb6f60e2e5828028e
HectorProjectDAW <eleuteriodaw@gmail.com>
2025-05-19 10:11:19 +0200

    Update TematicaMongo.java
    19/5/25
    10:10

--- TematicaMongo.java
+++ TematicaMongo.java
@@ -33,4 +33,31 @@ public class TematicaMongo {
             return lista.subList(0, Math.min(cantidad, lista.size()));
         }
     }
+    
+    
+    
+    //Lo mismo q arriba pero con palabras y no con tematicas
+    public static String palabraRandom(String tematica) {
+        String uri = "mongodb://root:alumnoalumno@localhost:27017/";
+
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("AHORCADO_TEMATICA");
+            MongoCollection<Document> coleccion = database.getCollection("tematicas");
+
+            // Buscar el documento con el nombre de la temática
+            Document filtro = new Document("nombre", tematica);
+            Document documento = coleccion.find(filtro).first();
+
+            if (documento != null) {
+                List<String> palabras = documento.getList("palabras", String.class);
+
+                //Aleatorizar
+                if (palabras != null && !palabras.isEmpty()) {
+                    Collections.shuffle(palabras);
+                    return palabras.get(0); 
+                }
+            }
+        }
+        return "sin_palabra"; 
+    }
 }
