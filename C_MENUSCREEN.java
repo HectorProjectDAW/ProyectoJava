760c1b37c65b16644de8432b920270d565568240
HectorProjectDAW <eleuteriodaw@gmail.com>
2025-05-21 11:52:36 +0200

    Update C_MENUSCREEN.java

--- C_MENUSCREEN.java
+++ C_MENUSCREEN.java
@@ -1,10 +1,8 @@
 package com.proyecto.mi_proyecto;
-import java.awt.EventQueue;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+
+import java.awt.*;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -27,72 +25,99 @@ public class C_MenuPrincipalScreen extends JFrame {
 	}
 
 	public C_MenuPrincipalScreen() {
-		setBackground(new Color(128, 0, 0));
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		setUndecorated(true); 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 500, 350);
 		setTitle("Menú Principal");
 
-		contentPane = new JPanel();
-		contentPane.setBackground(new Color(192, 192, 192));
-		contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));
+		contentPane = new JPanel(new GridBagLayout()); 
+		contentPane.setBackground(new Color(230, 230, 250)); // Lavanda claro
+		contentPane.setBorder(new EmptyBorder(20, 20, 20, 20));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
 
-		JButton btnPartidaNueva = new JButton("Nueva partida");
-		btnPartidaNueva.setForeground(Color.BLACK);
-		btnPartidaNueva.setBounds(160, 40, 180, 30);
-		contentPane.add(btnPartidaNueva);
-		btnPartidaNueva.addActionListener(new ActionListener() {
-		    public void actionPerformed(ActionEvent e) {
-		    	D_SelectorTemasScreen selecTema = new D_SelectorTemasScreen();
-		    	selecTema.setVisible(true);
-		    	dispose();
-		    }
-		});
+		Font fuenteBoton = new Font("SansSerif", Font.BOLD, 18);
+
 		
+		JLabel titulo = new JLabel("Menú Principal");
+		titulo.setFont(new Font("SansSerif", Font.BOLD, 28));
+		titulo.setForeground(new Color(60, 60, 60));
+		GridBagConstraints gbcTitulo = new GridBagConstraints();
+		gbcTitulo.gridx = 0;
+		gbcTitulo.gridy = 0;
+		gbcTitulo.insets = new Insets(20, 0, 30, 0);
+		contentPane.add(titulo, gbcTitulo);
 
-		JButton btnContinuarPartida = new JButton("Continuar partida");
-		btnContinuarPartida.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-		    }
-		});
-		btnContinuarPartida.setForeground(Color.BLACK);
-		btnContinuarPartida.setBounds(160, 80, 180, 30);
-		contentPane.add(btnContinuarPartida);
-
-		JButton btnDesafio = new JButton("Desafío");
-		btnDesafio.setForeground(Color.BLACK);
-		btnDesafio.setBounds(160, 120, 180, 30);
-		contentPane.add(btnDesafio);
-
-		JButton btnTienda = new JButton("Tienda");
-		btnTienda.setForeground(Color.BLACK);
-		btnTienda.setBounds(160, 160, 180, 30);
-		contentPane.add(btnTienda);
-
-		JButton btnRanking = new JButton("Ranking");
-		btnRanking.setForeground(Color.BLACK);
-		btnRanking.setBounds(160, 200, 180, 30);
-		contentPane.add(btnRanking);
 		
-		JButton btnSalirAlEscritorio = new JButton("Salir al escritorio");
-		btnSalirAlEscritorio.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-			}
+		JPanel panelBotones = new JPanel(new GridLayout(0, 1, 20, 20)); 
+		panelBotones.setBackground(new Color(255, 255, 255));
+		panelBotones.setBorder(BorderFactory.createEmptyBorder(40, 150, 40, 150));
+
+		JButton btnPartidaNueva = crearBoton("Nueva partida", fuenteBoton, e -> {
+			D_SelectorTemasScreen selecTema = new D_SelectorTemasScreen();
+			selecTema.setVisible(true);
+			dispose();
 		});
-		btnSalirAlEscritorio.setBounds(339, 259, 141, 46);
-		contentPane.add(btnSalirAlEscritorio);
+		panelBotones.add(btnPartidaNueva);
+
+		JButton btnContinuarPartida = crearBoton("Continuar partida", fuenteBoton, e -> {
+			
+		});
+		panelBotones.add(btnContinuarPartida);
+
+		JButton btnDesafio = crearBoton("Desafío", fuenteBoton, e -> {
+			
+		});
+		panelBotones.add(btnDesafio);
+
+		JButton btnTienda = crearBoton("Tienda", fuenteBoton, e -> {
+			
+		});
+		panelBotones.add(btnTienda);
+
+		JButton btnRanking = crearBoton("Ranking", fuenteBoton, e -> {
+			
+		});
+		panelBotones.add(btnRanking);
+
+		GridBagConstraints gbcBotones = new GridBagConstraints();
+		gbcBotones.gridx = 0;
+		gbcBotones.gridy = 1;
+		gbcBotones.anchor = GridBagConstraints.CENTER;
+		contentPane.add(panelBotones, gbcBotones);
+
 		
-		JButton btnDeslogearte = new JButton("Cerrar sesión");
-		btnDeslogearte.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				A_LoginScreen login = new A_LoginScreen();
-				login.setVisible(true);
-				dispose();
-			}
+		JPanel panelInferior = new JPanel(new BorderLayout(20, 0));
+		panelInferior.setBackground(new Color(255, 255, 255));
+		panelInferior.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+
+		JButton btnDeslogearte = crearBoton("Cerrar sesión", fuenteBoton, e -> {
+			A_LoginScreen login = new A_LoginScreen();
+			login.setVisible(true);
+			dispose();
 		});
-		btnDeslogearte.setBounds(12, 259, 141, 46);
-		contentPane.add(btnDeslogearte);
+		panelInferior.add(btnDeslogearte, BorderLayout.WEST);
+
+		JButton btnSalir = crearBoton("Salir al escritorio", fuenteBoton, e -> {
+			System.exit(0);
+		});
+		panelInferior.add(btnSalir, BorderLayout.EAST);
+
+		GridBagConstraints gbcInferior = new GridBagConstraints();
+		gbcInferior.gridx = 0;
+		gbcInferior.gridy = 2;
+		gbcInferior.insets = new Insets(40, 0, 0, 0);
+		contentPane.add(panelInferior, gbcInferior);
+	}
+
+	private JButton crearBoton(String texto, Font fuente, ActionListener accion) {
+		JButton boton = new JButton(texto);
+		boton.setFont(fuente);
+		boton.setBackground(new Color(100, 149, 237)); 
+		boton.setForeground(Color.WHITE);
+		boton.setFocusPainted(false);
+		boton.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+		boton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		boton.addActionListener(accion);
+		return boton;
 	}
 }
