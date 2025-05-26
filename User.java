587289a251c4d0c5700a67f2a29166cab362a331
HectorProjectDAW <eleuteriodaw@gmail.com>
2025-05-26 09:21:12 +0200

    Update User.java

--- User.java
+++ User.java
@@ -5,15 +5,32 @@ import java.security.NoSuchAlgorithmException;
 import java.sql.*;
 
 public class User {
+	
+	private static String username;
 
+    public static void iniciarSesion(String usuario) {
+        username = usuario;
+    }
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static void cerrarSesion() {
+        username = null;
+    }
+
+    //Validar user y contraseña
     public static boolean validarCredenciales(String usuario, String contrasenya) {
         if (usuario.isEmpty() || contrasenya.isEmpty()) return false;
 
         String hashedPassword = hashPassword(contrasenya);
 
+        //Prueba conexion a mysql a una BDD que se llama Login
         try (Connection conexion = DriverManager.getConnection(
                 "jdbc:mysql://localhost:33306/Login", "root", "alumnoalumno")) {
 
+        	
             String checkQuery = "SELECT * FROM Usuarios WHERE nombre_usuario = ?";
             try (PreparedStatement statement = conexion.prepareStatement(checkQuery)) {
                 statement.setString(1, usuario);
@@ -31,6 +48,8 @@ public class User {
         return false;
     }
 
+    
+    //Lo mismo que antes, mira si existe el user
     public static boolean existeUsuario(String usuario) {
         try (Connection conexion = DriverManager.getConnection(
                 "jdbc:mysql://localhost:33306/Login", "root", "alumnoalumno")) {
@@ -48,6 +67,7 @@ public class User {
         return false;
     }
 
+    //Igual, pero con el correo
     public static boolean existeCorreo(String correo) {
         try (Connection conexion = DriverManager.getConnection(
                 "jdbc:mysql://localhost:33306/Login", "root", "alumnoalumno")) {
@@ -65,6 +85,7 @@ public class User {
         return false;
     }
 
+    //Registra el usuario
     public static boolean registrarUsuario(String nombreUsuario, String correo, String contrasenya) {
         String hashedPassword = hashPassword(contrasenya);
 
@@ -85,6 +106,7 @@ public class User {
         }
     }
 
+    //Pone la contraseña en hashPassword
     private static String hashPassword(String password) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-256");
